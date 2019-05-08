a332895e0edc882ebee085f244a005339e7f6201
Kevin Krato <kratok08@gmail.com>
2019-05-07 20:34:17 -0600

    Implement base styles

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -1,15 +1,14 @@
 import React from "react";
-import NavBar from "../NavBar/NavBar";
+import SplashImage from "../SplashImage/SplashImage";
 import Carousel from "../Carousel/Carousel";
+import "./_MainPage.scss";
 
 class MainPage extends React.Component {
   render() {
     return (
-      <div>
-        <NavBar />
-        MainPage component
-        <Carousel />
-      </div>
+      <main class="main-page">
+        <SplashImage />
+      </main>
     );
   }
 }
