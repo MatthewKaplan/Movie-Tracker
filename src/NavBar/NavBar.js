858d24bc20b8ebaa4c39d0b320b19959e1c76916
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-05-07 22:16:26 -0600

    Fetch user search

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,14 +1,27 @@
 import React from "react";
 import "./NavBar.css";
+import { fetchMovieTvData } from "../api/index";
 
 class NavBar extends React.Component {
+  state = { searchUrl: "https://api.themoviedb.org/3/search/movie?" };
+
   searchChangeHandler = event => {
     const searchTerm = event.target.value;
     this.performSearch(searchTerm);
   };
 
-  performSearch = searchTerm => {
-    console.log(searchTerm);
+  performSearch = async searchTerm => {
+    const { searchUrl } = this.state;
+    try {
+      const searchedMovies = await fetchMovieTvData(
+        searchUrl,
+        "&query=" + searchTerm
+      );
+    } catch (error) {
+      this.setState({
+        error: error
+      });
+    }
   };
 
   render() {
