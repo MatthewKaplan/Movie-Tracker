88f2ccc12b21558cecd145c8f18d1fe34cb41e99
Kevin Krato <kratok08@gmail.com>
2019-05-07 20:32:53 -0600

    Build SplashImage component

---
+++ src/SplashImage/SplashImage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./_SplashImage.scss";
+
+class SplashImage extends React.Component {
+  render() {
+    return <div className="splash-image">IMAGE</div>;
+  }
+}
+
+export default SplashImage;
