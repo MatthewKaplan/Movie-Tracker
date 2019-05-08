9641ccd43c35d96a285f46ccb013542c5f1d6bba
Kevin Krato <kratok08@gmail.com>
2019-05-08 14:41:10 -0600

    Refactor App and remove login import

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import NavBar from "../NavBar/NavBar";
 import MainPage from "../MainPage/MainPage";
-import LoginPage from "../LoginPage/LoginPage";
 
 import { Route } from "react-router-dom";
 
@@ -11,9 +10,7 @@ function App() {
     <div className="App">
       <NavBar />
       <MainPage />
-
       <Route exact path="/" component={MainPage} />
-      <Route path="/login" component={LoginPage} />
     </div>
   );
 }
