f009f170f969c84ccc9ddb2f8d79f7e42ad4baf5
Kevin Krato <kratok08@gmail.com>
2019-05-07 20:33:45 -0600

    Migrate out navlinks

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
+import NavBar from "../NavBar/NavBar";
 import MainPage from "../MainPage/MainPage";
+import LoginPage from "../LoginPage/LoginPage";
+
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <MainPage />
+
+      <Route exact path="/" component={MainPage} />
+      <Route path="/login" component={LoginPage} />
     </div>
   );
 }
