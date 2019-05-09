31636a4dc912400ec42efbbf0bae2557f273e671
Kevin Krato <kratok08@gmail.com>
2019-05-09 14:54:53 -0600

    Render login page

--- src/containers/LoginPage/LoginPage.js
+++ src/containers/LoginPage/LoginPage.js
@@ -1,8 +1,31 @@
 import React from "react";
+import "./_LoginPage.scss";
 
 class LoginPage extends React.Component {
   render() {
-    return <div>LoginPage component</div>;
+    return (
+      <div className="login-page">
+        <section className="login-container">
+          <h1>Sign in</h1>
+          <form>
+            <div className="inputs">
+              <label for="email">Email:</label>
+              <input type="text" name="email" placeholder="Email..." />
+              <label for="password">Password: </label>
+              <input type="text" name="password" placeholder="Password..." />
+            </div>
+            <div className="buttons">
+              <div className="local-logins">
+                <button className="local-login">Sign in</button>
+                <button className="local-login">Register</button>
+              </div>
+              <button>Sign in with Google</button>
+              <button>Sign in with FaceBook</button>
+            </div>
+          </form>
+        </section>
+      </div>
+    );
   }
 }
 
