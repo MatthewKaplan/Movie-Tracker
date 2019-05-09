10f837a7d11daf21cd82d5b146bcee821ac79dee
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-05-08 21:45:21 -0600

    Fix double app.js

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,15 +2,17 @@ import React, { Component } from "react";
 import "./App.css";
 import NavBar from "../NavBar/NavBar";
 import MainPage from "../MainPage/MainPage";
-// import { fetchMovieTvData } from "../api/index";
-import { Route } from "react-router-dom";
+import GenrePage from "../GenrePage/GenrePage";
+import LoginPage from "../LoginPage/LoginPage";
+import { Switch, Router, Route } from "react-router-dom";
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <NavBar />
-        <MainPage />
+        <Route path="/" component={NavBar} />
+        <Route exact path="/" component={MainPage} />
       </div>
     );
   }
