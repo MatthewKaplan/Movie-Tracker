5f3868a302b873439c365342760139cd06fcfe90
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-05-10 14:26:56 -0600

    Create 404 page when user visits a wrong path'

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,24 +3,28 @@ import "./App.css";
 import NavBar from "../NavBar/NavBar";
 import MainPage from "../MainPage/MainPage";
 import GenrePage from "../GenrePage/GenrePage";
+import NotFound from "../NotFound/NotFound";
 import LoginPage from "../LoginPage/LoginPage";
-import { Switch, Router, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/" component={NavBar} />
-        <Route exact path="/" component={MainPage} />
-        <Route path="/ActionMovies" component={GenrePage} />
-        <Route path="/ComedyMovies" component={GenrePage} />
-        <Route path="/Documentaries" component={GenrePage} />
-        <Route path="/FamilyMovies" component={GenrePage} />
-        <Route path="/HorrorMovies" component={GenrePage} />
-        <Route path="/RomanceMovies" component={GenrePage} />
-        <Route path="/SearchResults" component={GenrePage} />
-        <Route exact path="/home" component={MainPage} />
-        <Route exact path="/login" component={LoginPage} />
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route path="/ActionMovies" component={GenrePage} />
+          <Route path="/ComedyMovies" component={GenrePage} />
+          <Route path="/Documentaries" component={GenrePage} />
+          <Route path="/FamilyMovies" component={GenrePage} />
+          <Route path="/HorrorMovies" component={GenrePage} />
+          <Route path="/RomanceMovies" component={GenrePage} />
+          <Route path="/SearchResults" component={GenrePage} />
+          <Route exact path="/home" component={MainPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route path="" component={NotFound} />
+        </Switch>
       </div>
     );
   }
---
+++ src/containers/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './NotFound.scss';
+
+const NotFound = () => (
+
+  <div className="notFound">
+  </div>
+
+)
+
+export default NotFound;
