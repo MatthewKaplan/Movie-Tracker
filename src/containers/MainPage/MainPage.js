3d8ff300041ce566397721725737f8868ea79379
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-05-14 22:35:15 -0600

    Test MainPage mapStateToProps and mapDispatchToProps

--- src/containers/MainPage/MainPage.js
+++ src/containers/MainPage/MainPage.js
@@ -202,7 +202,7 @@ MainPage.propTypes = {
   fetchGenres: PropTypes.func
 };
 
-const mapStateToProps = state => {
+export const mapStateToProps = state => {
   return {
     movies: state.movies,
     tv: state.tv,
@@ -212,7 +212,7 @@ const mapStateToProps = state => {
   };
 };
 
-const mapDispatchToProps = dispatch => ({
+export const mapDispatchToProps = dispatch => ({
   fetchPopularMovies: popularMovies =>
     dispatch(fetchPopularMovies(popularMovies)),
   fetchPopularTv: popularTv => dispatch(fetchPopularTv(popularTv)),
