6e6217b43de316186cf00148effa3de7746adff3
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-05-15 02:16:55 -0600

    Test MainPage

--- src/containers/MainPage/MainPage.test.js
+++ src/containers/MainPage/MainPage.test.js
@@ -3,6 +3,11 @@ import { shallow } from "enzyme";
 import { MainPage, mapStateToProps, mapDispatchToProps } from "./MainPage";
 import * as actions from "../../actions/index";
 import MockData from "../../assets/mockData";
+import { fetchData } from "../../apiCalls/apiCalls";
+import { nytApiKey } from "../../api/nytApiKey";
+import { apiKey } from "../../api/apiKey";
+
+jest.mock("../../apiCalls/apiCalls.js");
 
 let mockMoviesProps = MockData.popularMovies;
 let mockTvProps = MockData.popularShows;
@@ -13,7 +18,10 @@ let mockUpcoming = MockData.upcomingMovies;
 let mockLocation = jest.fn();
 
 describe("MainPage", () => {
-  let wrapper;
+  let wrapper, instance;
+  const path = "&with_genres=28";
+
+  fetchData.mockImplementation(() => Promise.resolve(1));
 
   beforeEach(() => {
     wrapper = shallow(
@@ -27,6 +35,11 @@ describe("MainPage", () => {
         upcoming={mockUpcoming}
       />
     );
+    instance = wrapper.instance();
+  });
+
+  afterEach(() => {
+    fetchData.mockClear();
   });
 
   it("should match the snapshot", () => {
@@ -39,6 +52,65 @@ describe("MainPage", () => {
       newsUrl: "https://api.nytimes.com/svc",
       showPopUp: false
     });
+  });
+
+  it("should invoke 'renderNewsResults, fetchPopularMovies, fetchPopularTvShows, fetchComingSoon' when mounted", () => {
+    jest.spyOn(instance, "renderNewsResults");
+    jest.spyOn(instance, "fetchPopularMovies");
+    jest.spyOn(instance, "fetchPopularTvShows");
+    jest.spyOn(instance, "fetchComingSoon");
+    instance.componentDidMount();
+    expect(instance.renderNewsResults).toHaveBeenCalled();
+    expect(instance.fetchPopularMovies).toHaveBeenCalled();
+    expect(instance.fetchPopularTvShows).toHaveBeenCalled();
+    expect(instance.fetchComingSoon).toHaveBeenCalled();
+  });
+
+  it("should call 'fetchData' with correct params when fetchPopularMovies is invoked", () => {
+    instance.fetchPopularMovies();
+    expect(fetchData).toHaveBeenCalledWith(
+      wrapper.state("movieUrl") +
+        "/discover/movie?" +
+        apiKey +
+        "&sort_by=popularity.desc"
+    );
+  });
+
+  it("should call 'fetchData' with correct params when fetchPopularTvShows is invoked", () => {
+    instance.fetchPopularTvShows();
+    expect(fetchData).toHaveBeenCalledWith(
+      wrapper.state("movieUrl") +
+        "/discover/tv?" +
+        apiKey +
+        "&sort_by=popularity.desc"
+    );
+  });
+
+  it("should call 'fetchData' with correct params when fetchComingSoon is invoked", () => {
+    instance.fetchComingSoon();
+    expect(fetchData).toHaveBeenCalledWith(
+      wrapper.state("movieUrl") + "/movie/upcoming?" + apiKey
+    );
+  });
+
+  it("should call 'fetchData' with correct params when renderNewsResults is invoked", () => {
+    instance.renderNewsResults();
+    expect(fetchData).toHaveBeenCalledWith(
+      wrapper.state("newsUrl") + "/topstories/v2/movies.json?" + nytApiKey
+    );
+  });
+
+  it("should invoke 'handleClick' when genre button is pressed with correct path", () => {
+    jest.spyOn(instance, "handleClick");
+    wrapper.find("[data-test='genre-click-event']").simulate("click");
+    expect(instance.handleClick).toHaveBeenCalledWith(path);
+  });
+
+  it("should call 'fetchData' with correct params when handleClick is invoked", () => {
+    instance.handleClick(path);
+    expect(fetchData).toHaveBeenCalledWith(
+      wrapper.state("movieUrl") + "/discover/movie?" + apiKey + path + "&page=3"
+    );
   });
 });
 
