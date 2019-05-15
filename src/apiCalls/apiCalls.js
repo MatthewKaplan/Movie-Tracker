0946a5251d8ce052f552edc6c6868c0009725837
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-05-15 00:32:04 -0600

    Test LoginPage

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,35 +1,31 @@
-const fetchData = (url) => {
-  return fetch(url)
-    .then(response => {
-      if(!response.ok) {
-        throw new Error("Fetch failed")
-      } else {
-        return response.json()
-      }}
-    )
-}
+const fetchData = url => {
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error("Fetch failed");
+    } else {
+      return response.json();
+    }
+  });
+};
 
-const fetchUserData = (url) => {
-  return fetch(url)
-    .then(response => {
-      if(!response.ok) {
-        throw new Error("Fetch failed")
-      } else {
-        return response.json()
-      }}
-    )
-}
+const fetchUserData = url => {
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error("Fetch failed");
+    } else {
+      return response.json();
+    }
+  });
+};
 
 const fetchPost = (url, options) => {
-  return fetch(url, options)
-    .then(response => {
-      if(!response.ok) {
-        throw new Error("unable to favorite")
-      } else {
-        return response.json()
-      }}
-    )
-}
-
+  return fetch(url, options).then(response => {
+    if (!response.ok) {
+      throw new Error("unable to favorite");
+    } else {
+      return response.json();
+    }
+  });
+};
 
 export { fetchPost, fetchData, fetchUserData };
--- src/containers/LoginPage/LoginPage.test.js
+++ src/containers/LoginPage/LoginPage.test.js
@@ -2,7 +2,10 @@ import React from "react";
 import { shallow } from "enzyme";
 import { LoginPage, mapDispatchToProps, mapStateToProps } from "./LoginPage";
 import { setUser, favoritesList, isLoggedIn } from "../../actions/index";
+import { fetchUserData, fetchPost } from "../../apiCalls/apiCalls";
 import MockData from "../../assets/mockData";
+
+jest.mock("../../apiCalls/apiCalls.js");
 
 const mockUser = { id: 1, name: "Matthew" };
 const mockFavorites = MockData.favoritedMovies;
@@ -10,9 +13,16 @@ const mockIsLoggedIn = true;
 
 describe("LoginPage", () => {
   let wrapper;
+  fetchUserData.mockImplementation(() => Promise.resolve(1));
+  fetchPost.mockImplementation(() => Promise.resolve(1));
 
   beforeEach(() => {
-    wrapper = shallow(<LoginPage />);
+    wrapper = shallow(<LoginPage user={mockUser} />);
+  });
+
+  afterEach(() => {
+    fetchUserData.mockClear();
+    fetchPost.mockClear();
   });
 
   it("should match the snapshot", () => {
@@ -28,21 +38,88 @@ describe("LoginPage", () => {
       error: ""
     });
   });
+
+  it("should change the state of name when user inputs name", () => {
+    expect(wrapper.state("name")).toEqual("");
+    let nameInput = { target: { value: "Matthew Kaplan", classList: "name" } };
+    wrapper.find("input.name").simulate("change", nameInput);
+    expect(wrapper.state("name")).toEqual("MATTHEW KAPLAN");
+  });
+
+  it("should change the state of email when user inputs email", () => {
+    expect(wrapper.state("email")).toEqual("");
+    let emailInput = {
+      target: { value: "kaplan.matthew@gmail.com", classList: "email" }
+    };
+    wrapper.find("input.email").simulate("change", emailInput);
+    expect(wrapper.state("email")).toEqual("KAPLAN.MATTHEW@GMAIL.COM");
+  });
+
+  it("should change the state of password when user inputs password", () => {
+    expect(wrapper.state("password")).toEqual("");
+    let passwordInput = {
+      target: { value: "mypassword", classList: "password" }
+    };
+    wrapper.find("input.password").simulate("change", passwordInput);
+    expect(wrapper.state("password")).toEqual("MYPASSWORD");
+  });
+
+  it("should call fetchPost with correct params when user registers", () => {
+    const url = "http://localhost:3000/api/users/new";
+    wrapper.setState({
+      name: "MATTHEW",
+      email: "KAPLAN.MATTHEW@GMAIL.COM",
+      password: "MYPASSWORD"
+    });
+    wrapper
+      .find("button.register")
+      .simulate("click", { preventDefault: () => {} });
+    const mockData = wrapper.state();
+    const userOptionObject = {
+      method: "POST",
+      body: JSON.stringify(mockData),
+      headers: { "Content-Type": "application/json" }
+    };
+    expect(fetchPost).toHaveBeenCalledWith(url, userOptionObject);
+  });
+
+  it("should call fetchPost with correct params when user signs in", () => {
+    const url = "http://localhost:3000/api/users";
+    wrapper.setState({
+      name: "MATTHEW",
+      email: "KAPLAN.MATTHEW@GMAIL.COM",
+      password: "MYPASSWORD"
+    });
+    wrapper.find("button.login").simulate("click", { preventDefault: () => {} });
+    const mockData = wrapper.state();
+    const userOptionObject = {
+      method: "POST",
+      body: JSON.stringify(mockData),
+      headers: { "Content-Type": "application/json" }
+    };
+    expect(fetchPost).toHaveBeenCalledWith(url, userOptionObject);
+  });
+
+  it("should call fetchUserData", () => {
+    wrapper.instance().getFavoriteMovies();
+    expect(fetchUserData).toHaveBeenCalled();
+  });
+
 });
 
 describe("mapStateToProps", () => {
   it("should return an object", () => {
     const mockData = {
       user: {}
-    }
+    };
     const expected = {
       user: {}
-    }
+    };
 
-    const mockprops = mapStateToProps(mockData)
-    expect(mockprops).toEqual(expected)
-  })
-})
+    const mockprops = mapStateToProps(mockData);
+    expect(mockprops).toEqual(expected);
+  });
+});
 
 describe("mapDispatchToProps", () => {
   it("should call dispatch for setUser", () => {
