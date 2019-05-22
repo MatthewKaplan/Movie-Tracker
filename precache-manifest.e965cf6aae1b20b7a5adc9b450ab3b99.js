9b5db0ddfa324c9baf9bc66b372e819180c40eec
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-05-22 11:59:44 -0600

    Updates

---
+++ precache-manifest.e965cf6aae1b20b7a5adc9b450ab3b99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd7c4261a99c6eb2b5c8d84f543c2ae",
+    "url": "/Movie-Tracker/index.html"
+  },
+  {
+    "revision": "554d2eef12462fb13304",
+    "url": "/Movie-Tracker/static/css/main.ffa5c65b.chunk.css"
+  },
+  {
+    "revision": "0570ce8864f982abdee5",
+    "url": "/Movie-Tracker/static/js/2.70601c7f.chunk.js"
+  },
+  {
+    "revision": "554d2eef12462fb13304",
+    "url": "/Movie-Tracker/static/js/main.eb67307b.chunk.js"
+  },
+  {
+    "revision": "f804ddde2dd9dc8fdf2b",
+    "url": "/Movie-Tracker/static/js/runtime~main.51f8b371.js"
+  },
+  {
+    "revision": "a575856737062e2788ef563e539fce3d",
+    "url": "/Movie-Tracker/static/media/arrow.a5758567.svg"
+  },
+  {
+    "revision": "3eaf65eb08fa62fa9d069813a2da035c",
+    "url": "/Movie-Tracker/static/media/favorite.3eaf65eb.svg"
+  },
+  {
+    "revision": "306e4f47e9bacdd9507337e151f0971a",
+    "url": "/Movie-Tracker/static/media/favoriteActive.306e4f47.svg"
+  }
+]);
